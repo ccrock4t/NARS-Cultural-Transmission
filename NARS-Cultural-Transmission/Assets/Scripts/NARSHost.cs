2e0b6d66776c51e6ecda2024a6c556aafac4cda6
ccrock4t <15344554+ccrock4t@users.noreply.github.com>
2022-05-17 01:10:54 -0400

    initial commit

---
+++ NARS-Cultural-Transmission/Assets/Scripts/NARSHost.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+
+public class NARSHost : MonoBehaviour
+{
+    public enum NARSType : int
+    {
+        NARS, ONA
+    }
+
+    public NARSType type;
+    NARSSensorimotor _sensorimotor;
+    Process process = null;
+    StreamWriter inputStreamWriter;
+
+    StreamWriter statisticsStreamwriter;
+
+
+    //UI output text
+    string lastOperationTextForUI = "";
+    bool operationUpdated = false;
+
+    //Babbling
+    float babbleTimer = 0;
+    public int babblesRemaining = 0;
+  
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+        
+        switch (type)
+        {
+            case NARSType.NARS:
+                LaunchNARS();
+                babblesRemaining = 120;
+                break;
+            case NARSType.ONA:
+                LaunchONA();
+                break;
+            default:
+                break;
+        }
+
+        _sensorimotor = GetComponent<NARSSensorimotor>();
+        _sensorimotor.SetNARSHost(this);
+
+        //_sensorimotor.TeachInitialKnowledge();
+    }
+
+    private NARSSensorimotor GetSensorimotor()
+    {
+        return _sensorimotor;
+    }
+
+    private void Update()
+    {
+        if (operationUpdated)
+        {
+            string text = "";
+            switch (type)
+            {
+                case NARSType.NARS:
+                    text = "NARS Operation:\n";
+                    break;
+                case NARSType.ONA:
+                    text = "ONA Operation:\n";
+                    break;
+                default:
+                    break;
+            }
+            text += lastOperationTextForUI;
+            operationUpdated = false;
+        }
+
+        if (type == NARSType.NARS)
+        {
+            babbleTimer -= Time.deltaTime;
+            if (babblesRemaining > 0 && babbleTimer <= 0f)
+            {
+                Babble();
+                babbleTimer = 1.0f;
+                babblesRemaining--;
+            }
+        }
+
+    }
+
+    void Babble()
+    {
+        int randInt = Random.Range(1, 4);
+        string input = "";
+        string op = "";
+        if (randInt == 1)
+        {
+            op = @"^moveForward";
+        }
+        else if (randInt == 2)
+        {
+            op = @"^lookLeft";
+        }
+        else if (randInt == 3)
+        {
+            op = @"^lookRight";
+        }
+
+        GetSensorimotor().SetOp(op);
+
+        input = "<(*,{SELF}) --> " + op + ">. :|:";
+
+        if (input != "")
+        {
+            this.AddInput(input);
+        }
+    }
+
+    public void LaunchONA()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo(@"cmd.exe");
+        startInfo.WorkingDirectory = Application.dataPath + @"\NARS";
+        startInfo.UseShellExecute = false;
+        startInfo.RedirectStandardInput = true;
+        startInfo.RedirectStandardOutput = true;
+        startInfo.RedirectStandardError = true;
+
+        process = new Process();
+        process.StartInfo = startInfo;
+        process.EnableRaisingEvents = true;
+        process.OutputDataReceived += new DataReceivedEventHandler(ONAOutputReceived);
+        process.ErrorDataReceived += new DataReceivedEventHandler(ErrorReceived);
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        process.StandardInput.WriteLine("NAR shell");
+        process.StandardInput.Flush();
+
+        inputStreamWriter = process.StandardInput;
+        AddInput("*volume=0");
+    }
+
+    public void LaunchNARS()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe");
+        startInfo.WorkingDirectory = Application.dataPath + @"\NARS";
+        startInfo.UseShellExecute = false;
+        startInfo.RedirectStandardInput = true;
+        startInfo.RedirectStandardOutput = true;
+        startInfo.RedirectStandardError = true;
+
+        process = new Process();
+        process.StartInfo = startInfo;
+        process.EnableRaisingEvents = true;
+        process.OutputDataReceived += new DataReceivedEventHandler(NARSOutputReceived);
+        process.ErrorDataReceived += new DataReceivedEventHandler(ErrorReceived);
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        process.StandardInput.WriteLine("java -Xmx1024m -jar opennars.jar");
+        process.StandardInput.Flush();
+
+        inputStreamWriter = process.StandardInput;
+        AddInput("*volume=0");
+    }
+
+    public void AddInferenceCycles(int cycles)
+    {
+        AddInput("" + cycles);
+    }
+
+    public void AddInput(string message)
+    {
+        //UnityEngine.Debug.Log("SENDING INPUT: " + message);
+
+        inputStreamWriter.WriteLine(message);
+    }
+    void NARSOutputReceived(object sender, DataReceivedEventArgs eventArgs)
+    {
+        //UnityEngine.Debug.Log(eventArgs.Data);
+        if (eventArgs.Data.Contains("EXE:")) //operation executed
+        {
+            //UnityEngine.Debug.Log("RECEIVED OUTPUT: " + eventArgs.Data);
+            int length = eventArgs.Data.IndexOf("(") - eventArgs.Data.IndexOf("^");
+            string operation = eventArgs.Data.Substring(eventArgs.Data.IndexOf("^"), length);
+            //UnityEngine.Debug.Log("RECEIVED OUTPUT: " + operation);
+
+            GetSensorimotor().SetOp(operation);
+        }
+
+        if (eventArgs.Data.Contains("EXE:") || eventArgs.Data.Contains("Executed"))
+        {
+            if (statisticsStreamwriter == null)
+            {
+                statisticsStreamwriter = File.CreateText("decisionlog.txt");
+            }
+            statisticsStreamwriter.WriteLine(eventArgs.Data);
+        }
+
+    }
+
+    void ONAOutputReceived(object sender, DataReceivedEventArgs eventArgs)
+    {
+        UnityEngine.Debug.Log(eventArgs.Data);
+        if (eventArgs.Data.Contains("executed with args")) //operation executed
+        {
+            string operation = eventArgs.Data.Split(' ')[0];
+            UnityEngine.Debug.Log("RECEIVED OUTPUT: " + operation);
+
+            GetSensorimotor().SetOp(operation);
+        }
+
+/*        if (eventArgs.Data.Contains("executed with args") || eventArgs.Data.Contains("decision"))
+        {
+            if (statisticsStreamwriter == null)
+            {
+                statisticsStreamwriter = File.CreateText("onadecisionlog.txt");
+            }
+            statisticsStreamwriter.WriteLine(eventArgs.Data);
+        }*/
+
+    }
+
+    void ErrorReceived(object sender, DataReceivedEventArgs eventArgs)
+    {
+        UnityEngine.Debug.LogError(eventArgs.Data);
+    }
+
+    void OnApplicationQuit()
+    {
+        if (process != null || !process.HasExited )
+        {
+            process.CloseMainWindow();
+        }
+        statisticsStreamwriter.Close();
+    }
+
+}
+
+
+
---
+++ NARS-Cultural-Transmission/Assets/Scripts/NARSSensorimotor.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NARSSensorimotor : MonoBehaviour
+{
+    private NARSHost _host;
+
+    Camera bodyCamera;
+    CharacterController bodyController;
+
+    float TIMER_DURATION = 0.33f; // how often to send inputs to NARS
+    float timer = 0;
+
+    List<GameObject> visibleObjs;
+    Queue<string> inputQueue = new Queue<string>();
+
+    public float rotatingAngle = 0f;
+    public Vector3 moveVector = Vector3.zero;
+
+    string opToExecute = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = TIMER_DURATION;
+
+        bodyCamera = GetComponentInChildren<Camera>();
+        bodyController = GetComponent<CharacterController>();
+    }
+
+    public void SetNARSHost(NARSHost host)
+    {
+        _host = host;
+    }
+
+    public NARSHost GetNARSHost()
+    {
+        return _host;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Narsese events
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+
+            //DrawRays(true);
+            RemindGoal();
+
+            timer = TIMER_DURATION;
+        }
+
+        if (GetNARSHost().type == NARSHost.NARSType.ONA)
+        {
+            GetNARSHost().AddInferenceCycles(1);
+        }
+
+        if (GetNARSHost().type == NARSHost.NARSType.NARS)
+        {
+            GetNARSHost().AddInferenceCycles(2);
+        }
+
+        //execute queued op if it exists
+        if (opToExecute.Length > 0)
+        {
+            char s = opToExecute[1];
+            opToExecute = char.ToUpper(s) + opToExecute.Substring(2);
+            Invoke(opToExecute, 0f);
+            opToExecute = "";
+        }
+
+        //update body
+        MoveBody();
+
+    }
+
+    void QueueInput(string input)
+    {
+        GetNARSHost().AddInput(input);
+        //inputQueue.Enqueue(input);
+    }
+
+    void MoveBody()
+    {
+        bodyController.SimpleMove(moveVector);
+        bodyController.transform.Rotate(0, rotatingAngle, 0);
+    }
+
+    // Perception
+
+    private void FixedUpdate()
+    {
+        DrawRays(false);
+    }
+
+    void DrawRays(bool LIDARSense)
+    {
+        int[] azimuth_degrees = new int[] { 0, 45, 90, 135, 180, 225, 270, 315 };
+        int[] altitude_degrees = new int[] { -90, -45, 0, 45, 90 };
+        float[] azimuth_radians = azimuth_degrees.Select(d => d * Mathf.Deg2Rad).ToArray();
+        float[]  altitude_radians = altitude_degrees.Select(d => d * Mathf.Deg2Rad).ToArray();
+
+
+        foreach (float azimuthX in azimuth_radians)
+        {
+            foreach (float altitude in altitude_radians)
+            {
+                foreach (float azimuthZ in azimuth_radians)
+                {
+                    // Does the ray intersect any objects excluding the player layer
+                    Vector3 rotation = new Vector3(Mathf.Cos(azimuthX), Mathf.Cos(altitude), Mathf.Cos(azimuthZ));
+                    int length = 10;
+                   // Debug.Log(rotation);
+                    Debug.DrawRay(transform.position, transform.TransformDirection(rotation)  * length, Color.green);
+
+                    if (LIDARSense)
+                    {
+                        RaycastHit hit;
+                        // Does the ray intersect any objects excluding the player layer
+                        if (Physics.Raycast(transform.position, transform.TransformDirection(rotation), out hit, Mathf.Infinity))
+                        {
+                            Debug.DrawRay(transform.position, transform.TransformDirection(rotation) * hit.distance, Color.red);
+                            //Debug.Log("Did Hit");
+                        }
+                        else
+                        {
+                            //Debug.Log("Did not Hit");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    public void TeachInitialKnowledge()
+    {
+        //GetNARSHost().AddInput("<(&/,<(*,{SELF},{$sth}) --> seesLeft>,^lookLeft) =/> <(*,{SELF},{$sth}) --> seesCenter>>.");
+        //GetNARSHost().AddInput("<(&/,<(*,{SELF},{$sth}) --> seesRight>,^lookRight) =/> <(*,{SELF},{$sth}) --> seesCenter>>.");
+        //GetNARSHost().AddInput("<(&/,<(*,{SELF},{$sth}) --> seesCenter>,^moveForward) =/> <(*,{SELF},{$sth}) --> touching>>.");
+        //GetNARSHost().AddInput("<(&/,(--,<(*,{SELF},{$sth}) --> sees>),^lookRight) =/> <(*,{SELF},{$sth}) --> sees>>.");
+    }
+
+
+    // ============== Motor Functions (OPERATIONS) ============== 
+    // NOTE: Function names must match NARS operation names
+    public void SetOp(string op)
+    {
+        opToExecute = op;
+    }
+
+    float moveSpeed = 1.5f, rotateSpeed = 0.66f;
+
+    public void LookLeft()
+    {
+        rotatingAngle = -rotateSpeed;
+        moveVector = Vector3.zero;
+    }
+
+    public void LookRight()
+    {
+        rotatingAngle = rotateSpeed;
+        moveVector = Vector3.zero;
+    }
+
+    public void MoveForward()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        moveVector = forward * moveSpeed;
+        rotatingAngle = 0;
+    }
+
+    // Rewards
+
+    public void RemindGoal()
+    {
+        //TODO
+        string goal = "<{SELF} --> [good]>! :|:";
+        QueueInput(goal);
+    }
+
+    public void Punish()
+    {
+        Debug.Log("BAD NARS");
+        //NARSHost.GetInstance().AddInput("(--,<{SELF} --> [good]>). :|:");
+        //NARSHost.GetInstance().AddInput("<{SELF} --> [bad]>. :|:");
+    }
+
+    public void Praise()
+    {
+        Debug.Log("GOOD " + GetNARSHost().type.ToString());
+        QueueInput("<{SELF} --> [good]>. :|:");
+    }
+}
